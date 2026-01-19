cb73e15249518dd4da06c38351895e9626f369cf
Phạm Thuỳ Trinh <trinhpham@Trinhs-MacBook-Pro.local>
2026-01-19 22:46:06 +0700

    hoan thanh bai 4 lab 4

---
+++ lab4/src/components/bai4-lab4.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">Kiến thức sức khỏe cộng đồng</h1>
+        
+        <div class="row text-start">
+            <div class="col-sm-4">
+                <div class="card p-0">
+                    <img :src="items[0].image" class="card-img-top" alt="Hình ảnh" />
+                    <div class="card-body">
+                        <h3 class="card-title">{{ items[0].title }}</h3>
+                        <p class="card-text">{{ items[0].content }}</p>
+                        <button class="btn btn-info">Xem chi tiết </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card p-0">
+                    <img :src="items[1].image" class="card-img-top" alt="Hình ảnh" />
+                    <div class="card-body">
+                        <h3 class="card-title">{{ items[1].title }}</h3>
+                        <p class="card-text">{{ items[1].content }}</p>
+                        <button class="btn btn-info">Xem chi tiết </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card p-0">
+                    <img :src="items[2].image" class="card-img-top" alt="Hình ảnh" />
+                    <div class="card-body">
+                        <h3 class="card-title">{{ items[2].title }}</h3>
+                        <p class="card-text">{{ items[2].content }}</p>
+                        <button class="btn btn-info">Xem chi tiết </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import img1 from '../assets/images/rau-cu.jpg';
+import img2 from '../assets/images/gia-vi.jpg';
+import img3 from '../assets/images/dau.jpg';
+
+const items = ([
+    { title: '8 loại rau củ quả giàu canxi', content: 'Canxi là khoáng chất cần thiết đối với cơ thể người. Có nhiều cách để bổ sung canxi, trong đó bổ sung qua đường ăn uống là cách tốt nhất. Có 8 loại rau củ giàu canxi...', image: img1},
+    { title: 'Các loại gia vị tốt cho sức khỏe', content: 'Một số loại gia vị cung cấp nhiều polyphenol chống oxy hóa cao hơn các loại gia vị quen thuộc khác. Với một lượng nhỏ các món ăn đã thêm hương thơm và vị hấp dẫn...', image: img2 },
+    { title: '9 loại đậu bổ dưỡng nên dùng nhiều', content: 'Đậu lăng, đậu nành, đậu phộng, đậu Hà Lan giàu chất xơ, protein cùng nhiều vitamin và khoáng chất giúp giảm lượng đường trong máu, tốt cho tim...', image: img3 },
+]);
+</script>
