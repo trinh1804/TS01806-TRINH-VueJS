3b2c1b8ab1bc4d3635cd179dd80f6b3f5dc7f3dc
Phạm Thuỳ Trinh <trinhpham@Trinhs-MacBook-Pro.local>
2026-01-29 23:12:41 +0700

    hoan thanh bai 1 lab 6

---
+++ lab6/src/components/Bai1-lab6.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mt-4">
+        <h3>Nhập điểm của bạn</h3>
+        <input type="number" v-model.number="score" />
+        <p v-if="score < 0 || score > 10" class="text-danger">Vui lòng nhập điểm từ 0 đến 10!</p>
+        <p v-else-if="score < 5">Yếu</p>
+        <p v-else-if="score >= 5 && score < 6.5">Trung bình</p>
+        <p v-else-if="score >= 6.5 && score < 8">Khá</p>
+        <p v-else-if="score >= 8 && score < 9">Giỏi</p>
+        <p v-else>Xuất sắc</p>
+
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const score = ref('0');
+</script>
