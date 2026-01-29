8e95a9037b50601693438563c6473e6bb7911bcd
Phạm Thuỳ Trinh <trinhpham@Trinhs-MacBook-Pro.local>
2026-01-29 23:13:42 +0700

    hoan thanh bai 3 lab 6

---
+++ lab6/src/components/Bai3-lab6.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="text-center">Kiến thức sức khỏe cộng đồng</h1>
+
+        <div class="row">
+            <div class="col-sm-4 mt-4" v-for="(item, index) in items" :key="index">
+                <div class="card p-0">
+                    <img :src="item.image" class="img-fluid" alt="Hình ảnh" />
+                    <div class="card-body">
+                        <h3 class="card-title">{{ item.title }}</h3>
+                        <p class="card-text">{{ item.content }}</p>
+                        <button class="btn btn-info">Xem chi tiết</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import img1 from '../assets/images/rau-cu.jpg';
+import img2 from '../assets/images/gia-vi.jpg';
+import img3 from '../assets/images/dau.jpg';
+
+const items = ([
+    { title: '8 loại rau củ quả giàu canxi', content: 'Canxi là khoáng chất cần thiết đối với cơ thể người. Có nhiều cách để bổ sung canxi, trong đó bổ sung qua đường ăn uống là cách tốt nhất. Có 8 loại rau củ giàu canxi...', image: img1 },
+    { title: 'Các loại gia vị tốt cho sức khỏe', content: 'Một số loại gia vị cung cấp nhiều polyphenol chống oxy hóa cao hơn các loại gia vị quen thuộc khác. Với một lượng nhỏ các món ăn đã thêm hương thơm và vị hấp dẫn...', image: img2 },
+    { title: '9 loại đậu bổ dưỡng nên dùng nhiều', content: 'Đậu lăng, đậu nành, đậu phộng, đậu Hà Lan giàu chất xơ, protein cùng nhiều vitamin và khoáng chất giúp giảm lượng đường trong máu, tốt cho tim...', image: img3 },
+]);
+</script>
