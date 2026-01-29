0be3ed36d61a3facf0a04a25248c94b10626377e
Phạm Thuỳ Trinh <trinhpham@Trinhs-MacBook-Pro.local>
2026-01-29 23:13:23 +0700

    hoan thanh bai 2 lab 6

---
+++ lab6/src/components/Bai2-lab6.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container mt-4">
+        <h3>Nhập tháng của bạn</h3>
+        <input type="number" v-model.number="month" />
+        <p v-if="month < 1 || month > 12" class="text-danger">Vui lòng nhập từ tháng 1 đến 12!</p>
+        <p v-else-if="month >= 1 && month <= 3">Mùa xuân</p>
+        <p v-else-if="month <= 6">Mùa hè</p>
+        <p v-else-if="month <= 9">Mùa thu</p>
+        <p v-else>Mùa đông</p>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const month = ref('1');
+</script>
+<style lang="">
+
+</style>
